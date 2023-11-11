efd572b93428e40c82a4a45f01e74fd259795f73
danneba <danielkassahun2@gmail.com>
2023-11-11 19:15:41 +0300

    changing the request

--- index.js
+++ index.js
@@ -49,40 +49,41 @@ app.get("/get-pg-version", async (req, res) => {
 //   });
 // });
 
-app.get("/convert-to-image", async (req, res) => {
-  // const htmlContent =
-  //   "<html><body><h1>Hello, HTML to Image!</h1></body></html>";
-  const htmlContent = path.join(__dirname, "views", "index.html");
-  fs.readFile(htmlContent, "utf8", async (err, htmlContent) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading the file");
-    }
-
-    try {
-      const image = await nodeHtmlToImage({ html: htmlContent });
-      res.writeHead(200, { "Content-Type": "image/png" });
-      res.end(image, "binary");
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error converting HTML to image");
-    }
-  });
-});
-
 // app.get("/convert-to-image", async (req, res) => {
-//   const htmlContent =
-//     "<html><body><h1>Hello, HTML to Image!</h1></body></html>";
+//   // const htmlContent =
+//   //   "<html><body><h1>Hello, HTML to Image!</h1></body></html>";
+//   const htmlContent = path.join(__dirname, "views", "index.html");
+//   fs.readFile(htmlContent, "utf8", async (err, htmlContent) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).send("Error reading the file");
+//     }
 
-//   try {
-//     const image = await nodeHtmlToImage({ html: htmlContent });
-//     res.writeHead(200, { "content-Type": "image/png" });
-//     res.end(image, "binary");
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Error converting HTML to  image");
-//   }
+//     try {
+//       const image = await nodeHtmlToImage({ html: htmlContent });
+//       res.writeHead(200, { "Content-Type": "image/png" });
+//       res.end(image, "binary");
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Error converting HTML to image");
+//     }
+//   });
 // });
+
+app.post("/convert-to-image", async (req, res) => {
+  const htmlContent = req.body.htmlBody;
+  if (!htmlContent) {
+    res.status(401).json({ error: "trying to connect " });
+  }
+  try {
+    const image = await nodeHtmlToImage({ html: htmlContent });
+    res.writeHead(200, { "content-Type": "image/png" });
+    res.end(image, "binary");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error converting HTML to  image");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
