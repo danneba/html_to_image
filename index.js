80e35b8388ee6c8273dff135dae201a8200578fb
danneba <danielkassahun2@gmail.com>
2023-11-12 14:01:31 +0300

    cors middleware

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const { error } = require("console");
 require("dotenv").config();
 
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 const port = process.env.PORT || 3000;
