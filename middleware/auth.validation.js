115c2083a64b6e0759181e04df0779d59c9fdea3
danneba <danielkassahun2@gmail.com>
2023-11-26 21:51:05 +0300

    validation result

--- middleware/auth.validation.js
+++ middleware/auth.validation.js
@@ -6,7 +6,7 @@ const loginValidation = [
 ];
 
 const validateLogin = (req, res, next) => {
-  const errors = validateResult(req);
+  const errors = validationResult(req);
 
   if (errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
